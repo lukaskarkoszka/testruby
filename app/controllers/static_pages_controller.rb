f0e991e6d50bb09324ab21022eae1a5ffbba364c
lukaskarkoszka <lukas.karkoszka@gmail.com>
2018-02-17 15:22:19 +0100

    add more acction in roots

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,7 @@
 class StaticPagesController < ApplicationController
+	def contact;end
+	def mision; end
+	def abaut; end
+	
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get '/test', to: proc {|e| [200,{},["ok"]]}
-  get '/users', to: "users#index" 
-  get '/contact', to: "contact#index"
+	get '/test', to: proc {|e| [200,{},["ok"]]}
+	get '/users', to: "users#index" 
+  	get '/contact', to: "static_pages#contact"
+	get '/about', to: "static_pages#about"
+	get '/mision', to: "static_pages#mision"
 end
